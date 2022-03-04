4d64fb576589b7d8b461659d9219b7a50e4d09cf
BeobSik <tjswl756@naver.com>
2022-03-04 15:22:54 +0900

    init project

---
+++ demoSpringCorePrinciple/src/main/java/com/example/demospringcoreprinciple/DemoSpringCorePrincipleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demospringcoreprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCorePrincipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCorePrincipleApplication.class, args);
+    }
+}
---
+++ demoSpringCorePrinciple/src/test/java/com/example/demospringcoreprinciple/DemoSpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringcoreprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCorePrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
