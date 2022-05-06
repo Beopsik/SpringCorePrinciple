8c2c49e68f6779e3893c4881b2fcea42c518dbaa
BeobSik <tjswl756@naver.com>
2022-05-06 15:37:20 +0900

    feat: @PostConstruct, @PreDestroy

--- demoSpringCorePrinciple/src/test/java/com/example/demospringcoreprinciple/lifecycle/NetworkClient.java
+++ demoSpringCorePrinciple/src/test/java/com/example/demospringcoreprinciple/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package com.example.demospringcoreprinciple.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
     private  String url;
 
@@ -28,12 +31,14 @@ public class NetworkClient{
         System.out.println("close: "+url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.clase");
         disconnect();
