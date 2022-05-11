6b969da51054e154b94f32d2b0f18cda32fe45b1
BeobSik <tjswl756@naver.com>
2022-05-11 16:05:55 +0900

    feat: 스코프와 Provider

---
+++ demoSpringCorePrinciple/src/main/java/com/example/demospringcoreprinciple/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package com.example.demospringcoreprinciple.web;
+
+import com.example.demospringcoreprinciple.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    //    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
